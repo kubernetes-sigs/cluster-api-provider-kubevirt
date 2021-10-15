ec0ede9805ab56f059d2893d24a20bf4d0ff3c14
Cheng Cheng <ccheng@ccheng.us>
2021-10-14 21:52:15 -0700

    The initial code migration for cluster-api-provider-kubevirt by Apple Inc.
    - Add utils packages (and unit tests) for machine controller
    
    Signed-off-by: Cheng Cheng <chengcheng@apple.com>

---
+++ pkg/kubevirt/machine.go
@@ -0,0 +1,169 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubevirt
+
+import (
+	gocontext "context"
+	"fmt"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	kubevirtv1 "kubevirt.io/client-go/api/v1"
+	"sigs.k8s.io/cluster-api-provider-kubevirt/pkg/context"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha4"
+	"sigs.k8s.io/cluster-api/util"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+// Machine implement a service for managing the KubeVirt VM hosting a kubernetes node.
+type Machine struct {
+	client         client.Client
+	machineContext *context.MachineContext
+	vmInstance     *kubevirtv1.VirtualMachineInstance
+}
+
+// NewMachine returns a new Machine service for the given context.
+func NewMachine(ctx *context.MachineContext, client client.Client) (*Machine, error) {
+	machine := &Machine{client, ctx, nil}
+
+	namespacedName := types.NamespacedName{Namespace: ctx.KubevirtMachine.Namespace, Name: ctx.KubevirtMachine.Name}
+	vmi := &kubevirtv1.VirtualMachineInstance{}
+
+	err := client.Get(ctx.Context, namespacedName, vmi)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return machine, nil
+		} else {
+			return nil, err
+		}
+	}
+
+	machine.vmInstance = vmi
+
+	return machine, nil
+}
+
+// Exists checks if the VM has been provisioned already.
+func (m *Machine) Exists() bool {
+	return m.vmInstance != nil
+}
+
+// Create creates a new VM for this machine.
+func (m *Machine) Create() error {
+	m.machineContext.Logger.Info(fmt.Sprintf("Creating VM with role '%s'...", nodeRole(m.machineContext)))
+
+	virtualMachine := newVirtualMachineFromKubevirtMachine(m.machineContext)
+
+	mutateFn := func() (err error) {
+		// Ensure the KubevirtMachine is marked as an owner of the VirtualMachine.
+		virtualMachine.SetOwnerReferences(util.EnsureOwnerRef(
+			virtualMachine.OwnerReferences,
+			metav1.OwnerReference{
+				APIVersion: m.machineContext.KubevirtMachine.APIVersion,
+				Kind:       m.machineContext.KubevirtMachine.Kind,
+				Name:       m.machineContext.KubevirtMachine.Name,
+				UID:        m.machineContext.KubevirtMachine.UID,
+			}))
+
+		// TODO: to remove those labels
+		if virtualMachine.Labels == nil {
+			virtualMachine.Labels = map[string]string{}
+		}
+		virtualMachine.Labels[clusterv1.ClusterLabelName] = "capk"
+
+		return nil
+	}
+	if _, err := controllerutil.CreateOrUpdate(gocontext.Background(), m.client, virtualMachine, mutateFn); err != nil {
+		return err
+	}
+
+	namespacedName := types.NamespacedName{Namespace: m.machineContext.KubevirtMachine.Namespace, Name: m.machineContext.KubevirtMachine.Name}
+	vmi := &kubevirtv1.VirtualMachineInstance{}
+	if err := m.client.Get(m.machineContext.Context, namespacedName, vmi); err != nil {
+		if apierrors.IsNotFound(err) {
+			return errors.New("failed to create VM instance")
+		}
+	}
+
+	return nil
+}
+
+// Address returns the IP address of the VM.
+func (m *Machine) Address() string {
+	if m.vmInstance != nil && len(m.vmInstance.Status.Interfaces) > 0 {
+		return m.vmInstance.Status.Interfaces[0].IP
+	}
+
+	return ""
+}
+
+// IsBooted checks if the VM is booted.
+func (m *Machine) IsBooted(executor CommandExecutor) bool {
+	if m.Address() == "" {
+		return false
+	}
+
+	output, err := executor.ExecuteCommand("hostname")
+	if err != nil || output != m.machineContext.KubevirtMachine.Name {
+		return false
+	}
+
+	return true
+}
+
+// IsBootstrapped checks if the VM is bootstrapped with Kubernetes.
+func (m *Machine) IsBootstrapped(executor CommandExecutor) bool {
+	if !m.IsBooted(executor) {
+		return false
+	}
+
+	output, err := executor.ExecuteCommand("cat /run/cluster-api/bootstrap-success.complete")
+	if err != nil || output != "success" {
+		return false
+	}
+
+	return true
+}
+
+// SetProviderID sets the KubeVirt provider ID for this kubernetes node.
+func (m *Machine) SetProviderID(workloadClusterClient client.Client) (string, error) {
+	if m.vmInstance == nil {
+		return "", errors.New("Underlying Kubevirt VM is NOT running")
+	}
+
+	providerID := fmt.Sprintf("kubevirt://%s", m.machineContext.KubevirtMachine.Name)
+
+	// using workload cluster client, get the corresponding cluster node
+	workloadClusterNode := &corev1.Node{}
+	workloadClusterNodeKey := client.ObjectKey{Namespace: m.machineContext.KubevirtMachine.Namespace, Name: m.machineContext.KubevirtMachine.Name}
+	if err := workloadClusterClient.Get(m.machineContext, workloadClusterNodeKey, workloadClusterNode); err != nil {
+		return "", errors.Wrapf(err, "failed to fetch workload cluster node")
+	}
+
+	// using workload cluster client, patch cluster node
+	patchStr := fmt.Sprintf(`{"spec": {"providerID": "%s"}}`, providerID)
+	mergePatch := client.RawPatch(types.MergePatchType, []byte(patchStr))
+	if err := workloadClusterClient.Patch(gocontext.TODO(), workloadClusterNode, mergePatch); err != nil {
+		return "", errors.Wrapf(err, "failed to patch workload cluster node")
+	}
+
+	return providerID, nil
+}
---
+++ pkg/ssh/utils.go
@@ -0,0 +1,161 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package ssh
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/ssh"
+)
+
+// generateKeys generates a pair of public and private keys
+func generateKeys() (pub, key []byte, err error) {
+	bitSize := 4096
+
+	privateKey, err := generatePrivateKey(bitSize)
+	if err != nil {
+		return nil, nil, err
+	}
+	privateKeyBytes := encodePrivateKeyToPEM(privateKey)
+
+	publicKeyBytes, err := generatePublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return publicKeyBytes, privateKeyBytes, nil
+}
+
+// generatePrivateKey creates a RSA Private Key of specified byte size
+func generatePrivateKey(bitSize int) (*rsa.PrivateKey, error) {
+	// create key
+	privateKey, err := rsa.GenerateKey(rand.Reader, bitSize)
+	if err != nil {
+		return nil, err
+	}
+
+	// validate key
+	err = privateKey.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+// encodePrivateKeyToPEM encodes Private Key from RSA to PEM format
+func encodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+	// get ASN.1 DER format
+	privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+
+	// pem.Block
+	privBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privDER,
+	}
+
+	// private key in PEM format
+	privatePEM := pem.EncodeToMemory(&privBlock)
+
+	return privatePEM
+}
+
+// generatePublicKey takes a rsa.PublicKey and returns bytes suitable for writing to .pub file
+// returns in the format "ssh-rsa ..."
+func generatePublicKey(privateKey *rsa.PublicKey) ([]byte, error) {
+	publicRsaKey, err := ssh.NewPublicKey(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKeyBytes := ssh.MarshalAuthorizedKey(publicRsaKey)
+
+	return pubKeyBytes, nil
+}
+
+func signerFromPem(pemBytes []byte, password []byte) (ssh.Signer, error) {
+	// read pem block
+	err := errors.New("Pem decode failed, no key found")
+	pemBlock, _ := pem.Decode(pemBytes)
+	if pemBlock == nil {
+		return nil, err
+	}
+
+	// handle encrypted key
+	if x509.IsEncryptedPEMBlock(pemBlock) {
+		// decrypt PEM
+		pemBlock.Bytes, err = x509.DecryptPEMBlock(pemBlock, []byte(password))
+		if err != nil {
+			return nil, fmt.Errorf("Decrypting PEM block failed %v", err)
+		}
+
+		// get RSA, EC or DSA key
+		key, err := parsePemBlock(pemBlock)
+		if err != nil {
+			return nil, err
+		}
+
+		// generate signer instance from key
+		signer, err := ssh.NewSignerFromKey(key)
+		if err != nil {
+			return nil, fmt.Errorf("Creating signer from encrypted key failed %v", err)
+		}
+
+		return signer, nil
+	} else {
+		// generate signer instance from plain key
+		signer, err := ssh.ParsePrivateKey(pemBytes)
+		if err != nil {
+			return nil, fmt.Errorf("Parsing plain private key failed %v", err)
+		}
+
+		return signer, nil
+	}
+}
+
+func parsePemBlock(block *pem.Block) (interface{}, error) {
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("Parsing PKCS private key failed %v", err)
+		} else {
+			return key, nil
+		}
+	case "EC PRIVATE KEY":
+		key, err := x509.ParseECPrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("Parsing EC private key failed %v", err)
+		} else {
+			return key, nil
+		}
+	case "DSA PRIVATE KEY":
+		key, err := ssh.ParseDSAPrivateKey(block.Bytes)
+		if err != nil {
+			return nil, fmt.Errorf("Parsing DSA private key failed %v", err)
+		} else {
+			return key, nil
+		}
+	default:
+		return nil, fmt.Errorf("Parsing private key failed, unsupported key type %q", block.Type)
+	}
+}
