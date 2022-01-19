0622335f80e8e00a6beba21451312acc5a65c61a
David Vossel <davidvossel@gmail.com>
2022-01-19 13:37:18 -0500

    Add e2e test validating VMs with persistent storage
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- e2e-tests/create-cluster_test.go
+++ e2e-tests/create-cluster_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -15,6 +16,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/rand"
+	kubevirtv1 "kubevirt.io/api/core/v1"
 	infrav1 "sigs.k8s.io/cluster-api-provider-kubevirt/api/v1alpha1"
 	tests "sigs.k8s.io/cluster-api-provider-kubevirt/e2e-tests"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
@@ -49,6 +51,7 @@ var _ = Describe("CreateCluster", func() {
 
 		clusterv1.AddToScheme(k8sclient.Scheme())
 		infrav1.AddToScheme(k8sclient.Scheme())
+		kubevirtv1.AddToScheme(k8sclient.Scheme())
 
 		namespace = "e2e-test-create-cluster-" + rand.String(6)
 
@@ -103,23 +106,55 @@ var _ = Describe("CreateCluster", func() {
 
 	})
 
-	It("creating a simple cluster", func() {
+	waitForBootstrappedMachines := func() {
+		Eventually(func() error {
+			machineList := &infrav1.KubevirtMachineList{}
+			err := k8sclient.List(context.Background(), machineList, client.InNamespace(namespace))
+			if err != nil {
+				return err
+			}
 
-		By("generating cluster manifests from example template")
-		cmd := exec.Command(tests.ClusterctlPath, "generate", "cluster", "kvcluster", "--target-namespace", namespace, "--kubernetes-version", "v1.21.0", "--control-plane-machine-count=1", "--worker-machine-count=1", "--from", "templates/cluster-template.yaml")
-		cmd.Env = append(os.Environ(),
-			"NODE_VM_IMAGE_TEMPLATE=quay.io/kubevirtci/fedora-kubeadm:35",
-			"IMAGE_REPO=k8s.gcr.io",
-			"CRI_PATH=/var/run/crio/crio.sock",
-		)
-		stdout, _ := tests.RunCmd(cmd)
-		err := os.WriteFile(manifestsFile, stdout, 0644)
-		Expect(err).To(BeNil())
+			for _, machine := range machineList.Items {
+				if !conditions.IsTrue(&machine, infrav1.BootstrapExecSucceededCondition) {
+					return fmt.Errorf("still waiting on a kubevirt machine with bootstrap succeeded condition")
+				}
+			}
+			return nil
+		}, 5*time.Minute, 5*time.Second).Should(BeNil(), "kubevirt machines should have bootstrap succeeded condition")
 
-		By("posting cluster manifests example template")
-		cmd = exec.Command(tests.KubectlPath, "apply", "-f", manifestsFile)
-		tests.RunCmd(cmd)
+	}
 
+	waitForMachineReadiness := func(numExpectedReady int, numExpectedNotReady int) {
+		Eventually(func() error {
+
+			readyCount := 0
+			notReadyCount := 0
+
+			machineList := &infrav1.KubevirtMachineList{}
+			err := k8sclient.List(context.Background(), machineList, client.InNamespace(namespace))
+			if err != nil {
+				return err
+			}
+
+			for _, machine := range machineList.Items {
+				if machine.Status.Ready {
+					readyCount++
+				} else {
+					notReadyCount++
+				}
+			}
+
+			if readyCount != numExpectedReady {
+				return fmt.Errorf("Expected %d ready, but got %d", numExpectedReady, readyCount)
+			} else if notReadyCount != numExpectedNotReady {
+				return fmt.Errorf("Expected %d not ready, but got %d", numExpectedNotReady, notReadyCount)
+			}
+
+			return nil
+		}, 5*time.Minute, 5*time.Second).Should(BeNil(), "waiting for expected readiness.")
+	}
+
+	waitForControlPlane := func() {
 		By("Waiting on cluster's control plane to initialize")
 		Eventually(func() error {
 			cluster := &clusterv1.Cluster{}
@@ -151,21 +186,82 @@ var _ = Describe("CreateCluster", func() {
 
 			return nil
 		}, 10*time.Minute, 5*time.Second).Should(BeNil(), "cluster should have control plane initialized")
+	}
+
+	It("creating a simple cluster with ephemeral VMs", func() {
+		By("generating cluster manifests from example template")
+		cmd := exec.Command(tests.ClusterctlPath, "generate", "cluster", "kvcluster", "--target-namespace", namespace, "--kubernetes-version", "v1.21.0", "--control-plane-machine-count=1", "--worker-machine-count=1", "--from", "templates/cluster-template.yaml")
+		cmd.Env = append(os.Environ(),
+			"NODE_VM_IMAGE_TEMPLATE=quay.io/kubevirtci/fedora-kubeadm:35",
+			"IMAGE_REPO=k8s.gcr.io",
+			"CRI_PATH=/var/run/crio/crio.sock",
+		)
+		stdout, _ := tests.RunCmd(cmd)
+		err := os.WriteFile(manifestsFile, stdout, 0644)
+		Expect(err).To(BeNil())
+
+		By("posting cluster manifests example template")
+		cmd = exec.Command(tests.KubectlPath, "apply", "-f", manifestsFile)
+		tests.RunCmd(cmd)
+
+		By("Waiting for control plane")
+		waitForControlPlane()
+
+		By("Waiting on kubevirt machines to bootstrap")
+		waitForBootstrappedMachines()
 
 		By("Waiting on kubevirt machines to be ready")
-		Eventually(func() error {
-			machineList := &infrav1.KubevirtMachineList{}
-			err = k8sclient.List(context.Background(), machineList, client.InNamespace(namespace))
-			if err != nil {
-				return err
-			}
+		waitForMachineReadiness(2, 0)
 
-			for _, machine := range machineList.Items {
-				if !conditions.IsTrue(&machine, infrav1.BootstrapExecSucceededCondition) {
-					return fmt.Errorf("still waiting on a kubevirt machine with bootstrap succeeded condition")
-				}
+	})
+
+	It("creating a simple cluster with persistent VMs", func() {
+		By("generating cluster manifests from example template")
+		cmd := exec.Command(tests.ClusterctlPath, "generate", "cluster", "kvcluster", "--target-namespace", namespace, "--kubernetes-version", "v1.21.0", "--control-plane-machine-count=1", "--worker-machine-count=1", "--from", "templates/cluster-template-persistent-storage.yaml")
+		cmd.Env = append(os.Environ(),
+			"NODE_VM_IMAGE_TEMPLATE=quay.io/kubevirtci/fedora-kubeadm:35",
+			"IMAGE_REPO=k8s.gcr.io",
+			"CRI_PATH=/var/run/crio/crio.sock",
+		)
+		stdout, _ := tests.RunCmd(cmd)
+		err := os.WriteFile(manifestsFile, stdout, 0644)
+		Expect(err).To(BeNil())
+
+		By("posting cluster manifests example template")
+		cmd = exec.Command(tests.KubectlPath, "apply", "-f", manifestsFile)
+		tests.RunCmd(cmd)
+
+		By("Waiting for control plane")
+		waitForControlPlane()
+
+		By("Waiting on kubevirt machines to bootstrap")
+		waitForBootstrappedMachines()
+
+		By("Waiting on kubevirt machines to be ready")
+		waitForMachineReadiness(2, 0)
+
+		By("Selecting a worker node to restart")
+		vmiList := &kubevirtv1.VirtualMachineInstanceList{}
+		err = k8sclient.List(context.Background(), vmiList, client.InNamespace(namespace))
+		Expect(err).To(BeNil())
+
+		var chosenVMI *kubevirtv1.VirtualMachineInstance
+		for _, vmi := range vmiList.Items {
+			if strings.Contains(vmi.Name, "-md-0") {
+				chosenVMI = &vmi
+				break
 			}
-			return nil
-		}, 5*time.Minute, 5*time.Second).Should(BeNil(), "kubevirt machines should have bootstrap succeeded condition")
+		}
+		Expect(chosenVMI).ToNot(BeNil())
+
+		By(fmt.Sprintf("By restarting worker node hosted in vmi %s", chosenVMI.Name))
+		err = k8sclient.Delete(context.Background(), chosenVMI)
+		Expect(err).To(BeNil())
+
+		By("Expecting a KubevirtMachine to revert back to ready=false while VM restarts")
+		waitForMachineReadiness(1, 1)
+
+		By("Expecting both KubevirtMachines stabilize to a ready=true again.")
+		waitForMachineReadiness(2, 0)
 	})
 })
