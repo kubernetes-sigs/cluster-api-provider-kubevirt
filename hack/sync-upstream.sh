8bd4fe1a3aecf9f0e37fe75788e1ad065529689f
David Vossel <davidvossel@gmail.com>
2022-04-20 13:42:50 -0400

    Update the sync script to use merge instead of rebase
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/sync-upstream.sh
+++ hack/sync-upstream.sh
@@ -1,58 +1,72 @@
 #!/bin/bash -ex
 
-# This script syncs the upstream repository kubernetes-sigs/cluster-api-provider-kubevirt, with the downstream
-# repository openshift/cluster-api-provider-kubevirt. It preserved the downstream files by backup these files
-# and restore them after the rebase.
+set -e
+
+
+# This script should be executed from within your fork of capk
 #
-# The script assumes 3 git remotes:
-# * origin - points to the user fork
-# * upstream - points to the kubernetes-sig organization
-# * openshift - points to the openshift organization
-# if there are no openshift or upstream remotes, the script add them
-#
-# also, the script assumes the the gh cli is installed and authorized (download from here: https://cli.github.com/).
-#
+# The script will...
+# 1. automatically create remote branches for upstream and the openshift capk fork
+# 2. create a new branch using the latest openshift capk fork's main
+# 3. Merge upstream/main into the new branch, which layers in any upstream changes.
+# 4. automatically resolve conflicts for expected file changes
+# 5. validate upstream and downstream diff only contains expected file changes
+# 6. push the new branch and automatically create a PR.
+# 
+# The script assumes the the gh cli is installed and authorized (download from here: https://cli.github.com/).
+# The gh cli is required in order to auto create the PR
+
+UPSTREAM_REMOTE="auto-sync-script-upstream"
+OPENSHIFT_REMOTE="auto-sync-script-openshift-fork"
+
+UPSTREAM_BRANCH="${UPSTREAM_REMOTE}/main"
+OPENSHIFT_BRANCH="${OPENSHIFT_REMOTE}/main"
+
+TODAY=$(date -u +%Y-%m-%d-%H-%M)
+NEW_BRANCH_NAME="auto_sync_upstream_${TODAY}"
+
+function verify_merge() {
+	echo "Verifying merge only has expected differences with upstream branch"
+
+	git checkout  $NEW_BRANCH_NAME
+
+	DS_FILES=(OWNERS hack/sync-upstream.sh)
+	diff=$(git diff upstream/main --stat | awk '{ print $1 }')
+
+	for file in ${diff[@]}; do
+	        # This if statment skips the final row of the diff --stat
+	        # that is just printing out the number of files that changed
+	        if [ "${#DS_FILES[@]}" = "$file" ]; then
+	                continue
+	        fi
+
+	        verified="false"
+	        for ds_file in ${DS_FILES[@]}; do
+	                if [ "$ds_file" = "$file" ]; then
+	                        verified="true"
+	                fi
+	        done
+	        if [ "$verified" = "false" ]; then
+	                echo "Unexpected diff, file $file should not be different from upstream"
+	                exit 1
+	        fi
+
+	        echo "verified diff file $file is allowed"
+	done
+}
+
 DS_FILES=(OWNERS hack/sync-upstream.sh)
-EXCLUDE_FILES=$(echo ${DS_FILES[@]} | tr ' ' '|')
-# get github user name
-GH_USER=$(gh auth status 2> >(grep "Logged in to github.com as" | sed -E "s/^.*Logged in to github.com as ([^ ]+).*$/\1/g"))
 
-# find the origin remote - the user fork
-OR=($(git remote -v | grep -E $"^[^${TAB} ]*${TAB}.*${GH_USER}/cluster-api-provider-kubevirt.git" | cut -f1 | uniq))
-ORIGIN=${OR[0]}
-if [[ -z ${ORIGIN} ]]; then
-  echo "the script requires a fork to your github user"
-  exit 1
-fi
+set +e
+git remote add $UPSTREAM_REMOTE https://github.com/kubernetes-sigs/cluster-api-provider-kubevirt.git
+git remote add $OPENSHIFT_REMOTE git@github.com:openshift/cluster-api-provider-kubevirt.git
+set -e
 
-# find the upstream remote. if not exist - creates it
-US=($(git remote -v | grep -E $"^[^${TAB} ]*${TAB}.*kubernetes-sigs/cluster-api-provider-kubevirt.git" | cut -f1 | uniq))
-UPSTREAM=${US[0]}
-if [[ -z ${UPSTREAM} ]]; then
-  git remote add upstream https://github.com/kubernetes-sigs/cluster-api-provider-kubevirt.git
-  UPSTREAM=upstream
-fi
+git fetch $UPSTREAM_REMOTE
+git fetch $OPENSHIFT_REMOTE
 
-# find the downstream remote, if not exists, create it
-OS=($(git remote -v | grep -E $"^[^${TAB} ]*${TAB}.*openshift/cluster-api-provider-kubevirt.git" | cut -f1 | uniq))
-OPENSHIFT=${OS[0]}
-if [[ -z ${OPENSHIFT} ]]; then
-  git remote add openshift https://github.com/openshift/cluster-api-provider-kubevirt.git
-  OPENSHIFT=openshift
-fi
-
-git fetch ${UPSTREAM} main
-git fetch ${OPENSHIFT} main
-
-if [[ $(git diff ${OPENSHIFT}/main ${UPSTREAM}/main --name-only | grep -vE "${EXCLUDE_FILES}" | wc -l) -eq 0 ]]; then
-  echo "no change found; aborting..."
-  exit 0
-fi
-
-TODAY=$(date -u +%Y-%m-%d)
-BRANCH_NAME="auto_sync_upstream_${TODAY}"
-git checkout -b ${BRANCH_NAME}
-git reset --hard "${OPENSHIFT}/main"
+git checkout $OPENSHIFT_BRANCH
+git checkout -b $NEW_BRANCH_NAME
 
 # backup the downstream files
 for ds_file in ${DS_FILES[@]}; do
@@ -60,36 +74,27 @@ for ds_file in ${DS_FILES[@]}; do
 done
 
 set +e
-git rebase ${UPSTREAM}/main | grep -E "^CONFLICT " > conflicts.txt
+git merge $UPSTREAM_BRANCH
 set -e
 
 # restore the downstream files, if they are conflicting
 for ds_file in ${DS_FILES[@]}; do
-  if grep "Merge conflict in ${ds_file}$" conflicts.txt; then
-    mv "${ds_file}.untracked" ${ds_file}
-    git add ${ds_file}
-  else
-    rm "${ds_file}.untracked"
-  fi
+  mv "${ds_file}.untracked" ${ds_file}
+  git add ${ds_file}
 done
 
-# continue rebase after fixing the conflicts
-if ! git -c core.editor=true rebase --continue; then
-  git rebase --abort
-  echo sync failed
-  exit 1
-fi
+git -c core.editor=true merge --continue
 
-rm conflicts.txt
+verify_merge
 
-# push the changes to own repository
-git push -u ${ORIGIN} ${BRANCH_NAME}
+git push origin $NEW_BRANCH_NAME
 
 # the gh CLI does not support choosing the remote repository from command line. Setting the default one
 sed -i "/gh-resolved = base/d" .git/config
-sed -i "/^\[remote \"${OPENSHIFT}\"\]/a \        gh-resolved = base" .git/config
+sed -i "/^\[remote \"${OPENSHIFT_REMOTE}\"\]/a \        gh-resolved = base" .git/config
 
 gh pr create -B main -t "auto sync ${TODAY}" -b "auto sync upstream ${TODAY}" -R openshift/cluster-api-provider-kubevirt
 
 # remove the default target repository, so gh cli will ask next time
 sed -i "/gh-resolved = base/d" .git/config
+
