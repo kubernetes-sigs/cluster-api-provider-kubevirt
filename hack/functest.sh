8b5a2b6434e17980a36d57c200731533610ea8dc
David Vossel <davidvossel@gmail.com>
2022-10-07 10:23:55 -0400

    update e2e to k8s 1.23.10 guest clusters
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/functest.sh
+++ hack/functest.sh
@@ -13,9 +13,9 @@ echo "Building and installing capk manager container"
 
 echo "Running e2e test suite"
 export KUBECONFIG=$(./kubevirtci kubeconfig)
-export NODE_VM_IMAGE_TEMPLATE=quay.io/capk/ubuntu-2004-container-disk:v1.22.0
 export IMAGE_REPO=k8s.gcr.io
-export TENANT_CLUSTER_KUBERNETES_VERSION=v1.22.0
+export TENANT_CLUSTER_KUBERNETES_VERSION="v1.23.10"
+export NODE_VM_IMAGE_TEMPLATE=quay.io/capk/ubuntu-2004-container-disk:${TENANT_CLUSTER_KUBERNETES_VERSION}
 export CRI_PATH=/var/run/containerd/containerd.sock
 export ROOT_VOLUME_SIZE=13Gi
 export STORAGE_CLASS_NAME=local
