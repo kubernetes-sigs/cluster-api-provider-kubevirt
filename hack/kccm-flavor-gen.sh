5d8762be2eac429a6f9236b5bce8ba5f425cbb56
Enrique Llorente <ellorent@redhat.com>
2022-11-23 11:03:34 +0100

    release: Add all the cluster template flavors
    There were missing template flavors at the github release used when a
    user generate a cluster with --infrastructure kubevirt, this change add
    all of the flavors and also check that the kccm ones are generated.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- hack/kccm-flavor-gen.sh
+++ hack/kccm-flavor-gen.sh
@@ -5,7 +5,7 @@ set -x -e -o pipefail
 kccm_template=/tmp/kccm.yaml
 
 kubectl kustomize config/kccm > $kccm_template
-for cluster_template in $(find templates/ -type f ! -name "*kccm*" -and ! -name "*ext*"); do
+for cluster_template in $(find templates/ -type f ! -name "*kccm*" -and ! -name "*ext*" -and ! -name "OWNERS"); do
     cluster_kccm_template=${cluster_template%%.*}-kccm.yaml
     cp -f $cluster_template ${cluster_kccm_template}
     echo "---" >> ${cluster_kccm_template}
