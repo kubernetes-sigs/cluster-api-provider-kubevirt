68fbea2854ccedb6d2c1f96db1fb2c778f310a7b
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2023-08-21 08:28:24 +0300

    Fix make generate-kccm-flavors
    `make generate-kccm-flavors` generates wrong manifests, setting a
    constant names where we should use variables.
    
    This commit fixes the `hack/kccm-flavor-gen.sh` script so it will
    replace the hard coded names with the right variables.
    
    Signed-off-by: Nahshon Unna-Tsameret <nunnatsa@redhat.com>

--- hack/kccm-flavor-gen.sh
+++ hack/kccm-flavor-gen.sh
@@ -5,6 +5,12 @@ set -x -e -o pipefail
 kccm_template=/tmp/kccm.yaml
 
 kubectl kustomize config/kccm > $kccm_template
+
+
+sed -i \
+    -E "s|(namespace: )kvcluster|\1\${NAMESPACE}|g;s|(^.*cluster-name=)kvcluster|\1\${CLUSTER_NAME}|g" \
+    ${kccm_template}
+
 for cluster_template in $(find templates/ -type f ! -name "*kccm*" -and ! -name "*ext*" -and ! -name "OWNERS"); do
     cluster_kccm_template=${cluster_template%%.*}-kccm.yaml
     cp -f $cluster_template ${cluster_kccm_template}
